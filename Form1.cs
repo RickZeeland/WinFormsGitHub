9d4ab2c643925a09638797f5b3ff5ec3c3e17701
RickZeeland <ramdepauw@gmail.com>
2022-08-08 19:08:55 +0200

    Added SerialPort test

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,15 @@
+using System.IO.Ports;
+
 namespace WinFormsGitHub
 {
     public partial class Form1 : Form
     {
+        public SerialPort serialPort;
+
         public Form1()
         {
             InitializeComponent();
+
         }
     }
 }
